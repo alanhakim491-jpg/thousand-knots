7cb0f99362671a577e2cd082f91f613ebf07b205
Alan Hakim <alanhakim491@gmail.com>
2025-11-20 14:16:12 +0200

    added sign-up page to children routes of login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { SignUp } from './pages/sign-up/sign-up';
 
 export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         component: Home,
+    },
+    {
+        path: 'login',
+        loadComponent: () => { return import('./pages/login/login').then((m) => m.Login) },
+        children: [
+            { path: 'sign-up', component: SignUp }
+        ],
     }
 ];
