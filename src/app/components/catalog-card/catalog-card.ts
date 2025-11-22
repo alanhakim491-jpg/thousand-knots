994a63121089ef428e1e230faae6fc0d5c51cc34
Alan Hakim <alanhakim491@gmail.com>
2025-11-22 16:38:00 +0200

    adjusted catalog card to include bookmark button

--- src/app/components/catalog-card/catalog-card.ts
+++ src/app/components/catalog-card/catalog-card.ts
@@ -1,19 +1,26 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, ViewChild, ElementRef } from '@angular/core';
 import { Catalog } from '../../services/catalog';
 import { Catalogs } from '../../models/catalog.types';
 import { CartButton } from '../buttons/cart-button/cart-button';
+import { MatIconModule } from '@angular/material/icon';
+import { ShovableBtn } from "../buttons/shovable-btn/shovable-btn";
 
 @Component({
   selector: 'app-catalog-card',
-  imports: [CartButton],
+  imports: [CartButton, MatIconModule, ShovableBtn],
   template: `
     <div class="catalog-card">
-      <div class="stock">
-        @if (product.sku) {
-          {{ product.sku }} left
-        } @else {
-          Out of stock
-        }
+      <div class="the-top">
+        <div class="stock">
+          @if (product.sku) {
+            {{ product.sku }} left
+          } @else {
+            Out of stock
+          }
+        </div>
+        <app-shovable-btn (btnClick)="markHandler()">
+          <mat-icon>{{ bookmarkIcon }}</mat-icon>
+        </app-shovable-btn>
       </div>
       <div class="the-image">
         <img [src]="product.imageURL" class="mb-2 rounded-lg"/>
@@ -21,7 +28,7 @@ import { CartButton } from '../buttons/cart-button/cart-button';
           <p>{{ product.description }}</p>
         </div>
       </div>
-      <div class="props flex flex-col justify-between gap-2">
+      <div class="props flex flex-col justify-between gap-3">
         <h2>{{ product.title }}</h2>
         <div class="the-bottom flex flex-row justify-between items-center">
           <p>{{ '$' + product.price  }}</p>
@@ -36,4 +43,10 @@ export class CatalogCard {
   catalogService = inject(Catalog);
 
   @Input() product!: Catalogs;
+
+  bookmarkIcon = 'bookmark_border';
+
+  markHandler() {
+    this.bookmarkIcon = this.bookmarkIcon === 'bookmark_border' ? 'bookmark' : 'bookmark_border';
+  }
 }
