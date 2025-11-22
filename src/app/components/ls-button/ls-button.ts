0b423c22f042fc608f9f78bc3af9a3291d392dec
Alan Hakim <alanhakim491@gmail.com>
2025-11-22 16:39:08 +0200

    glassmorphism; stylistic adjustment

--- src/app/components/ls-button/ls-button.ts
+++ src/app/components/ls-button/ls-button.ts
@@ -11,9 +11,17 @@ import { Component, input } from '@angular/core';
       font-size: 1.1rem;
       border-radius: 7px;
       box-shadow: -10px 10px 20px 5px rgba(255, 255, 255, 0.04), 10px -10px 20px 5px rgba(0, 0, 0, 0.08);
-      transition: background-color 0.3s ease-in-out;
+      backdrop-filter: blur(8px) saturate(100%);
+      -webkit-backdrop-filter: blur(8px) saturate(100%);
+      background: rgba(253, 244, 227, 0.08);
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      box-shadow: 
+        4px 4px 20px 0 rgba(120, 53, 15, 0.1),
+        inset 0 0 150px rgba(245, 158, 11, 0.05),
+        inset 0px 0px 4px 2px rgba(255, 255, 255, 0.15);
+      transition: border 0.3s ease-in-out;
       &:hover {
-        background-color: #f0f0f0;
+        border: white;
       }
       @media screen and (min-width: 1600px) {
         font-size: 1.3rem;
