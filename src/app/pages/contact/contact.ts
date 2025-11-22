afb9d2bbd4a6acec2dbcc066da53870db14ac67e
Alan Hakim <alanhakim491@gmail.com>
2025-11-22 16:48:15 +0200

    added and styled form section

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -6,12 +6,21 @@ import { LoginForm } from '../../components/login-form/login-form';
   imports: [LoginForm],
   template: `
     <app-login-form>
-      <div>
-        
+      <div class="contact-us">
+        <h1>Contact Us</h1>
+        <form>
+          <div class="top-row">
+            <input type="text" placeholder="Full Name*" required />
+            <input type="tel" id="phone" placeholder="Phone*" required />
+          </div>
+          <input type="email" placeholder="E-mail*" required />
+          <textarea placeholder="Tell Us!" rows="20" cols="67"></textarea>
+          <input type="submit" />
+        </form>
       </div>
     </app-login-form>
   `,
-  styles: ``,
+  styleUrls: ['./contact.scss'],
 })
 export class Contact {
 
