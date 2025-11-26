5f64713d9b0eb8b5be2afcc385fb18adf1f3c9b7
Alan Hakim <alanhakim491@gmail.com>
2025-11-26 19:58:29 +0200

    setup full signup page imported Router and ReactiveFormsModule and FormBuilder and Validators to setup a reactive form (with validation in the TS) As well as define a function to redirect the user back to login page upon form submission/signup

--- src/app/pages/sign-up/sign-up.ts
+++ src/app/pages/sign-up/sign-up.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginForm } from "../../components/login-form/login-form";
+import { LsButton } from 'src/app/components/ls-button/ls-button';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-sign-up',
-  imports: [LoginForm],
+  imports: [LoginForm, LsButton, ReactiveFormsModule],
   template: `
     <app-login-form>
-      
+      <h1>Sign Up</h1>
+      <form [formGroup]="signupForm" (ngSubmit)="onSubmit()">
+        <div class="up">
+          <input type="text" formControlName="fullName" placeholder="Full Name" required class="border-black border-1 rounded-lg h-9 w-[13.45vw] text-[1.1rem]" />
+          <input type="tel" formControlName="phone" placeholder="Phone" required class="border-black border-1 rounded-lg h-9 w-[13.45vw] text-[1.1rem]" />
+        </div>
+        <input type="email" formControlName="email" placeholder="E-mail" required class="border-black border-1 rounded-lg h-9 w-[28vw] text-[1.1rem]" />
+        <input type="password" formControlName="pass" placeholder="Password" required class="border-black border-1 rounded-lg h-9 w-[28vw] text-[1.1rem]" />
+        <input type="password" formControlName="confirmPass" placeholder="Confirm Password" required class="border-black border-1 rounded-lg h-9 w-[28vw] text-[1.1rem]" />
+        <app-ls-button [value]="'Sign-Up'" />
+      </form>
     </app-login-form>
   `,
   styleUrls: ['./sign-up.scss'],
 })
 export class SignUp {
 
+  private fb = inject(FormBuilder); // we could also create an instance using constructor(private fb: FormBuilder, private router: Router) {signupForm = ..}, 
+  private router = inject(Router); // however, inject is the modern angular 17-20+ way
+
+  signupForm = this.fb.group({  // import FormBuilder and ReactiveFormModule and Validators at top of component
+    fullName: ['', Validators.required], // add in the Template inside the input type, a formControlName="" to use in the TS class
+    phone: ['', Validators.required], // define the singupForm as an fb.group (FormBuilder.group) and set it in the form tag as [formGroup]="signupForm"
+    email: ['', Validators.email],
+    pass: ['', Validators.required],
+    confirmPass: ['', Validators.required]
+  })
+
+  onSubmit() { // define this function after the (ngSubmit) event
+    if (this.signupForm.valid) {
+      this.router.navigate(["/login"]);
+    }
+  }
 }
