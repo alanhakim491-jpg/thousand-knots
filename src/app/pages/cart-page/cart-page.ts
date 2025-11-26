1a9b49ade491a7c1ddd9797336469846f9f3b1c7
Alan Hakim <alanhakim491@gmail.com>
2025-11-26 11:07:37 +0200

    initialized and produced cart page, as well as defining cc-content's layout within it (ngClass)

--- src/app/pages/cart-page/cart-page.ts
+++ src/app/pages/cart-page/cart-page.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CcContent } from 'src/app/components/cc-content/cc-content';
+import { CartItem } from 'src/app/components/cart-item/cart-item';
+import { CcHeader } from 'src/app/components/cc-header/cc-header';
+import { Cart } from 'src/app/services/cart';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [CcContent],
+  imports: [CcContent, CcHeader, CartItem],
   template: `
-    <app-cc-content>
-      
+    <app-cc-header [header]="'Cart'" />
+    <app-cc-content layout="flex-col">
+      @for (item of cartService.cart(); track item.id) {
+        <app-cart-item [item]="item" />
+      }
     </app-cc-content>
   `,
   styles: ``,
 })
 export class CartPage {
 
+  cartService = inject(Cart);
+  
 }
