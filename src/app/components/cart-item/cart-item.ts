8350d9f45fe011cddd526ead6b9c4fe639c9641b
Alan Hakim <alanhakim491@gmail.com>
2025-11-26 13:19:55 +0200

    finalized the cart item with functionality and style, as well as used the reusable styling ngClass for the primary button

--- src/app/components/cart-item/cart-item.ts
+++ src/app/components/cart-item/cart-item.ts
@@ -1,16 +1,36 @@
-import { Component, input } from '@angular/core';
-import { Catalog } from 'src/app/services/catalog';
+import { Component, input, inject } from '@angular/core';
+import { Carts } from 'src/app/models/cart.types';
+import { Cart } from 'src/app/services/cart';
+import { PrimaryButton } from '../buttons/primary-button/primary-button';
+import { ShovableBtn } from '../buttons/shovable-btn/shovable-btn';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  imports: [PrimaryButton, ShovableBtn, MatIconModule],
   template: `
-    <p>
-      cart-item works!
-    </p>
+    <div class="cart-card">
+      <div class="left" >
+        <img [src]="item().imageURL" />
+        <div class="words">
+          <h2>{{ item().title }}</h2>
+          <p>{{ '$' + item().price }}</p>
+        </div>
+      </div>
+      <div class="right">
+        <div class="inc-dec">
+          <app-shovable-btn (btnClick)="cartService.decHandler(item().id)"><mat-icon>remove</mat-icon></app-shovable-btn>
+          <h2>{{ item().count }}</h2>
+          <app-shovable-btn (btnClick)="cartService.incHandler(item().id)"><mat-icon>add</mat-icon></app-shovable-btn>
+        </div>
+        <app-primary-button label='DELETE' class="text-[1.1rem]" effect="cart" (click)="cartService.delHandler(item().id)" />
+      </div>
+    </div>
   `,
-  styles: ``,
+  styleUrls: ['./cart-item.scss'],
 })
 export class CartItem {
-  item = input.required<Catalog>();
+  item = input.required<Carts>();
+
+  cartService = inject(Cart);
 }
