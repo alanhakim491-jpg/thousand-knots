4a135dea85ee8939b67de429c63d4642693bfd68
Alan Hakim <alanhakim491@gmail.com>
2025-11-26 11:04:32 +0200

    made cc content reusable with various layouts when its used in different pages. used ngClass and @Input

--- src/app/components/cc-content/cc-content.ts
+++ src/app/components/cc-content/cc-content.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common'; 
 
 @Component({
   selector: 'app-cc-content',
-  imports: [],
+  imports: [NgClass], // ngClass comes from @angular/common, if we ever wanna use it
   template: `
-    <div class="cc">
+    <div class="cc" [ngClass]="layout">
       <ng-content></ng-content>
     </div>
   `,
   styleUrls: ['./cc-content.scss'],
 })
 export class CcContent {
-
+  @Input() layout: 'grid' | 'flex-col' = 'grid'; // indicates an input, whith options, and if none are passed it defaults to 'grid'
 }
+
+// what we did here is we chose to make a variable class using input
+// it allowed us to bind the class to the input so as to make it variable based on where we're going to use this reusable container
+// and we imported ngClass to bind the layout input to be a class
+// we then took to the scss stylesheet and styled .cc.grid and .cc.flex-col to define the styles of the various instances
