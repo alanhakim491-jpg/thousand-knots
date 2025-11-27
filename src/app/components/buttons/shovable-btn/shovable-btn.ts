1655913cb34054e5c20cf7013ee96c48b0b990d8
Alan Hakim <alanhakim491@gmail.com>
2025-11-27 10:59:35 +0200

    added an @Input instance to change styling based on where its imported into

--- src/app/components/buttons/shovable-btn/shovable-btn.ts
+++ src/app/components/buttons/shovable-btn/shovable-btn.ts
@@ -1,22 +1,29 @@
-import { Component, output } from '@angular/core';
+import { Component, output, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-shovable-btn',
-  imports: [],
+  imports: [NgClass],
   template: `
-    <button (click)=btnClick.emit()>
+    <button (click)=btnClick.emit() [ngClass]="where">
       <ng-content></ng-content>
     </button>
   `,
   styles: `
     button {
       height: 2vh;
+      cursor: pointer;
+    }
+    button.header {
       width: 2vw;
       padding: 0.3vh 0.3vw;
-      cursor: pointer;
+    }
+    button.catalog {
+      padding: 0
     }
   `,
 })
 export class ShovableBtn {
   btnClick = output();
+  @Input() where: 'catalog' | 'header' = 'header';
 }
