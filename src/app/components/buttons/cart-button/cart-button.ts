27b97c7f14899eb55b1b961912f47b3d4117ef0f
Alan Hakim <alanhakim491@gmail.com>
2025-12-01 23:12:16 +0200

    used NgClass to make button configurable between catalog and cart

--- src/app/components/buttons/cart-button/cart-button.ts
+++ src/app/components/buttons/cart-button/cart-button.ts
@@ -1,10 +1,11 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-cart-button',
-  imports: [],
+  imports: [NgClass],
   template: `
-    <button (click)=btnClicked.emit()>{{ label() }}</button>
+    <button (click)=btnClicked.emit() [ngClass]="width">{{ label() }}</button>
   `,
   styles: `
     button {
@@ -12,13 +13,21 @@ import { Component, input, output } from '@angular/core';
       width: 100%;
       background-color: black;
       color: white;
-      padding: 0.5rem 0.7rem;
-      border: 0.5px solid black;
       transition: 0.3s ease;
       cursor: pointer;
       &:hover {
         scale: 1.05;
       }
+    }
+
+    button.catalog {
+      width: 100%;
+      padding: 0.5rem 0.7rem;
+    }
+    button.summary {
+      border-radius: 5px;
+      width: 27.5vw;
+      padding: 0.6rem 0.7rem
     }
   `,
 })
@@ -27,4 +36,5 @@ export class CartButton {
   label = input('');
   btnClicked = output();
 
+  @Input() width: 'catalog' | 'summary' = 'catalog';
 }
