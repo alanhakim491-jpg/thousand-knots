3e9b8653863942a15ab9ed84c6d4e2ff801e6db3
Alan Hakim <alanhakim491@gmail.com>
2025-12-01 15:14:35 +0200

    added a footer cvomponent

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <p>
+      © Thousand Knots 2025
+    </p>
+  `,
+  styles: `
+    p {
+      text-align: center;
+      font-size: 0.8rem;
+      font-weight: 300;
+      color: #666;
+      padding: 2vh 0;
+      margin: 0;
+    }
+  `,
+})
+export class Footer {
+
+}
