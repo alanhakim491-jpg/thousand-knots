17643e59382a4949d6908140529a818966583159
Alan Hakim <alanhakim491@gmail.com>
2025-12-01 14:27:06 +0200

    changed glassmorphic color scheme

--- src/app/components/buttons/ls-button/ls-button.ts
+++ src/app/components/buttons/ls-button/ls-button.ts
@@ -10,15 +10,14 @@ import { Component, input, output } from '@angular/core';
     input[type='submit'] {
       font-size: 1.1rem;
       border-radius: 7px;
-      box-shadow: -10px 10px 20px 5px rgba(255, 255, 255, 0.04), 10px -10px 20px 5px rgba(0, 0, 0, 0.08);
-      backdrop-filter: blur(8px) saturate(100%);
-      -webkit-backdrop-filter: blur(8px) saturate(100%);
-      background: rgba(253, 244, 227, 0.08);
+      backdrop-filter: blur(5px) saturate(120%);
+      -webkit-backdrop-filter: blur(5px) saturate(120%);
+      background: rgba(224, 242, 254, 0.1);
       border: 1px solid rgba(255, 255, 255, 0.2);
       box-shadow: 
-        4px 4px 20px 0 rgba(120, 53, 15, 0.1),
-        inset 0 0 150px rgba(245, 158, 11, 0.05),
-        inset 0px 0px 4px 2px rgba(255, 255, 255, 0.15);
+        0px 8px 24px 0 rgba(12, 74, 110, 0.15),
+        inset 0 0 0px rgba(255, 255, 255, 0),
+        inset 0px 0px 4px 2px rgba(255, 255, 255, 0.2);
       transition: scale 0.3s ease-in-out;
       &:hover {
         scale: 1.05;
