647e8e0bdfcdd3c1aaab043921af59f38b82bd0d
Alan Hakim <alanhakim491@gmail.com>
2025-12-06 10:40:49 +0200

    initialized the otem page

---
+++ src/app/pages/item-page/item-page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-page',
+  imports: [],
+  template: `
+    <div class="the-item">
+      <img />
+      <div class="the-details">
+        
+      </div>
+    </div>
+  `,
+  styles: ``,
+})
+export class ItemPage {
+
+}
